9a75c799438b640de301059128a5f48be97ae1c6
omarshahwan <o.shahwan@bareekmedia.com>
2025-03-08 20:00:31 +0300

    fix partner request type

--- src/components/partners/PartnerForm.tsx
+++ src/components/partners/PartnerForm.tsx
@@ -66,24 +66,25 @@ const PartnerForm = ({
   const { mutateAsync: addMutation, isPending: adding } = useCreatePartner();
 
   const onSubmit = (values: yup.InferType<typeof schema>) => {
-    console.log(values);
-    const payload: PartnerRequest = {
-      organization: {
-        name: values.name,
-        description: values.description,
-        enabled: values.enabled,
-        logoBase64: values.logoBase64,
-        type: {
-          name: "PARTNER",
+    const payload: PartnerRequest[] = [
+      {
+        organization: {
+          name: values.name,
+          description: values.description,
+          enabled: values.enabled,
+          logoBase64: values.logoBase64,
+          type: {
+            name: "PARTNER",
+          },
+          // ...values,
         },
-        // ...values,
+        users: values?.users?.map((user) => ({
+          firstName: user?.firstName,
+          lastName: user?.lastName,
+          email: user?.email,
+        })),
       },
-      users: values?.users?.map((user) => ({
-        firstName: user?.firstName,
-        lastName: user?.lastName,
-        email: user?.email,
-      })),
-    };
+    ];
     addMutation(payload);
   };
 
